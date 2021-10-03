44345393ba74c3b829595f525d7c5721d151389a
Blair Burke <BlairBurke@Outlook.com>
2021-10-02 22:18:30 -0230

    fixed card image gap

--- src/components/CountryCard/CountryCard.styled.ts
+++ src/components/CountryCard/CountryCard.styled.ts
@@ -3,8 +3,6 @@ import styled from "styled-components";
 export const CardContainer = styled.div`
   display: flex;
   flex-direction: column;
-  justify-content: center;
-  align-items: start;
   background: ${(props) => props.theme.colors.elements};
   box-shadow: 0 0 8px 0 rgba(0, 0, 0, 0.1);
   border-radius: 5px;
